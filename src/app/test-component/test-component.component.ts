774e36a6200cc80093b778bd615a9ad1cddc98a3
Nguyen Truong Minh <minhnt97@fpt.com>
2024-08-09 17:32:40 +0700

    sync

--- src/app/test-component/test-component.component.ts
+++ src/app/test-component/test-component.component.ts
@@ -6,6 +6,7 @@ export interface SelectItem {
   id: string;
   name: string;
   level?: number;
+  disabled?: boolean;
 }
 
 @Component({
@@ -71,47 +72,47 @@ export class TestComponent implements OnInit {
       level: 0,
     },
     {
-      id: '1.1',
+      id: '2.1',
       name: 'Biểu mẫu',
       level: 1,
     },
     {
-      id: '1.1.1',
+      id: '2.1.1',
       name: 'Table 1',
       level: 2,
     },
     {
-      id: '1.1.1.1',
+      id: '2.1.1.1',
       name: 'Value 7',
       level: 3,
     },
     {
-      id: '1.1.1.2',
+      id: '2.1.1.2',
       name: 'Value 8',
       level: 3,
     },
     {
-      id: '1.1.1.3',
+      id: '2.1.1.3',
       name: 'Value 9',
       level: 3,
     },
     {
-      id: '1.1.2',
+      id: '2.1.2',
       name: 'Table 2',
       level: 2,
     },
     {
-      id: '1.1.2.1',
+      id: '2.1.2.1',
       name: 'Value 10',
       level: 3,
     },
     {
-      id: '1.1.2.2',
+      id: '2.1.2.2',
       name: 'Value 11',
       level: 3,
     },
     {
-      id: '1.1.2.3',
+      id: '2.1.2.3',
       name: 'Value 12',
       level: 3,
     },
@@ -119,21 +120,39 @@ export class TestComponent implements OnInit {
   selectedItem: any;
 
   visibleItems: any[] = [];
+  visibleIndexMap: { [key: number]: number } = {};
   expandedMap: { [key: string]: boolean } = {};
 
   ngOnInit(): void {
     this.visibleItems = this.getVisibleItems();
+    this.lowestLevel = Math.min(...this.items.map((item) => item.level || 0));
   }
 
-  hasChildren(index: number): boolean {
-    const currentLevel = this.items[index].level || 0;
+  getVisibleItems(term?: string): SelectItem[] {
+    const visibleItems: SelectItem[] = [];
 
-    for (let i = index + 1; i < this.items.length; i++) {
+    for (const [index, option] of this.items.entries()) {
+      if (this.isExpanded(this.getParentIndex(index))) {
+        if (this.hasChildren(option)) {
+          option.disabled = true;
+        }
+        visibleItems.push(option);
+        this.visibleIndexMap[visibleItems.length - 1] = index;
+      }
+    }
+    console.log(visibleItems);
+    return visibleItems;
+  }
+
+  hasChildren(item: any): boolean {
+    const currentLevel = item.level || 0;
+    const currentItemIndex = this.items.findIndex((i) => i === item);
+
+    for (let i = currentItemIndex + 1; i < this.items.length; i++) {
       const itemLevel = this.items[i].level || 0;
-
       if (itemLevel === currentLevel + 1) {
         return true;
-      } else if (itemLevel <= currentLevel) {
+      } else {
         return false;
       }
     }
@@ -141,104 +160,88 @@ export class TestComponent implements OnInit {
     return false;
   }
 
-  getVisibleItems(): SelectItem[] {
-    const visibleItems: SelectItem[] = [];
-    let i = 0;
-    const levels = this.items
-      .filter((i) => !!i.level)
-      .map((i) => i.level) as number[];
-
-    while (i < Math.max(...levels)) {
-      const item = this.items[i];
-      const level = item.level || 0;
-      visibleItems.push(item);
-
-      // Add children if the current item is expanded
-      if (this.isExpanded(item.id)) {
-        while (
-          i + 1 < this.items.length &&
-          (this.items[i + 1].level || 0) > level
-        ) {
-          visibleItems.push(this.items[i + 1]);
-        }
-      } else {
-        // Skip children of the collapsed item
-        while (
-          i + 1 < this.items.length &&
-          (this.items[i + 1].level || 0) > level
-        ) {
-          i++;
-        }
-      }
-
-      i++;
-    }
-
-    return visibleItems;
-  }
+  lowestLevel: number = 0;
 
   trackByFn(item: any): string {
     return item.id;
   }
 
+  getParentIndex(index: number) {
+    for (let i = index; i >= 0; i--) {
+      const option = this.items[i];
+      if (
+        option.level != undefined &&
+        this.items[index].level &&
+        option.level < this.items[index].level
+      ) {
+        return i;
+      }
+    }
+    return undefined;
+  }
+
   toggleCollapse(index: number) {
-    const itemId = this.items[index].id;
-    this.expandedMap[itemId] = !this.expandedMap[itemId];
-    console.log(this.expandedMap);
+    this.expandedMap[this.visibleIndexMap[index]] =
+      !this.expandedMap[this.visibleIndexMap[index]];
     this.visibleItems = this.getVisibleItems();
   }
 
   debouncedToggleCollapse = debounce((index: number) => {
     this.toggleCollapse(index);
-  }, 300); // Adjust debounce time as needed
+  }, 0);
+
   debounceToggleCollapse(index: number, event: MouseEvent) {
     event.stopPropagation();
     this.debouncedToggleCollapse(index);
   }
 
-  isExpanded(itemId: string): boolean {
-    return !!this.expandedMap[itemId];
+  isExpanded(index?: number): boolean {
+    if (index == undefined) {
+      return true;
+    }
+    return !!this.expandedMap[index];
   }
 
   events: any[] = [];
 
   onChange($event: any) {
-    this.events.push({ name: '(change)', value: $event });
+    this.events.unshift({ name: '(change)', value: $event });
   }
 
   onFocus($event: any) {
-    this.events.push({ name: '(focus)', value: $event });
+    this.events.unshift({ name: '(focus)', value: $event });
   }
 
   onBlur($event: any) {
-    this.events.push({ name: '(blur)', value: $event });
+    this.events.unshift({ name: '(blur)', value: $event });
   }
 
   onOpen() {
-    this.events.push({ name: '(open)', value: null });
+    this.events.unshift({ name: '(open)', value: null });
   }
 
   onClose() {
-    this.events.push({ name: '(close)', value: null });
+    this.events.unshift({ name: '(close)', value: null });
   }
 
   onAdd($event: any) {
-    this.events.push({ name: '(add)', value: $event });
+    this.events.unshift({ name: '(add)', value: $event });
   }
 
   onRemove($event: any) {
-    this.events.push({ name: '(remove)', value: $event });
+    this.events.unshift({ name: '(remove)', value: $event });
   }
 
   onClear() {
-    this.events.push({ name: '(clear)', value: null });
+    this.events.unshift({ name: '(clear)', value: null });
   }
 
   onScrollToEnd($event: any) {
-    this.events.push({ name: '(scrollToEnd)', value: $event });
+    this.events.unshift({ name: '(scrollToEnd)', value: $event });
   }
 
   onSearch($event: any) {
-    this.events.push({ name: '(search)', value: $event });
+    this.events.unshift({ name: '(search)', value: $event });
+    this.visibleItems = this.getVisibleItems($event.term);
   }
 }
